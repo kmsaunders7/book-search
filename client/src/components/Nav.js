0f9c7feb63eee6794754efd1589c297be4eb31bb
kmsaunders7 <kmsaunders7@gmail.com>
2021-04-18 16:55:22 -0500

    add jumbotron and nav comp

---
+++ client/src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        Google Books
+      </a>
+      <div>
+          <ul className="collapse navbar-collapse" id="navbar">
+              <li className="nav-item">
+                  <a className="nav-link" href="/search"> Search </a>
+              </li>
+              <li className="nav-item">
+                  <a className="nav-link" href="/search"> Saved Books </a>
+              </li>
+          </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
